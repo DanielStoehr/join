86e170e055fa9a8c9e03ae6942e8fcc44f24bb16
LukasKlammer <klammer.lukas@hotmail.com>
2022-04-23 16:50:23 +0200

    backend integration in backlog

--- backlog/backlog.js
+++ backlog/backlog.js
@@ -1,13 +1,17 @@
-let users = [];
+let tasks = [];
 setURL('http://gruppe-223.developerakademie.net/smallest_backend_ever');
 
 async function init() {
     await downloadFromServer();
-    users = JSON.parse(backend.getItem('id')) || [];
+    tasks = JSON.parse(backend.getItem('task2')) || [];
+    console.log(tasks);
+    id = JSON.parse(backend.getItem('id')) || [];
+    console.log(id);
 }
 
 
-function addUser() {
-    users.push(username.value);
-    backend.setItem('id', JSON.stringify(users));
-}
+// don't needet at this time 
+// function addTask() {
+//     tasks.push(username.value);
+//     backend.setItem('id', JSON.stringify(tasks));
+// }
