ecd3d691f0fb18d445da9267b64b4a16168fbb11
Daniel Stoehr <daniel.stoehr@boschrexroth.de>
2022-05-09 07:03:10 +0200

    add burger menu to navigation

--- navigation/navigation.js
+++ navigation/navigation.js
@@ -1,7 +1,16 @@
 function changeNavigation(clickedElement) {
-    const links = document.getElementsByClassName('navigation-link')
-    for (const link of links) {
-        link.classList.remove('navigation-active')
-    }
-    clickedElement.classList.add('navigation-active')
+  const links = document.getElementsByClassName("navigation-link");
+  for (const link of links) {
+    link.classList.remove("navigation-active");
+  }
+  clickedElement.classList.add("navigation-active");
+}
+
+function toggleMenu() {
+  const navContainer = document.getElementById("navigation-link-container");
+  if (navContainer.style.display == "") {
+    navContainer.style.display = "flex";
+  } else {
+    navContainer.style = "";
+  }
 }
